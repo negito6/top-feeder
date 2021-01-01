f7210b84efcdd82fa7b50e3afd7958a6af2e3df3
Toru YAGI <negito6@users.noreply.github.com>
2021-01-01 15:08:10 +0900

    fish corner

--- src/html/js/ts/main.ts
+++ src/html/js/ts/main.ts
@@ -162,7 +162,7 @@ class Fish {
              .replace(/%{alpha}/, (this.appetite / 100).toString())
              .replace(/%{marginLeft}/, ((wMax - w) * this.x).toString())
              .replace(/%{marginTop}/, ((hMax - h) * this.z).toString())
-             .replace(/%{borderRadius}/,this.vx > 0 ? "0% 90% 0% 90%" : "90% 0% 90% 0%")
+             .replace(/%{borderRadius}/, (this.vx * this.vz > 0) ? "0% 90% 0% 90%" : "90% 0% 90% 0%")
              ;
   }
 }
